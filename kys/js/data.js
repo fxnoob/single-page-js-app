a3a06745a562cb29f7788a16f3e8b482c9d0d7d8
fxnoob <fxnoob71@gmail.com>
2018-09-13 01:48:39 +0530

    single page kys(know your student) app in js

---
+++ kys/js/data.js
@@ -0,0 +1,35 @@
+var data = [
+  {
+    "SerialNo": 1,
+    "Name": "name",
+    "Fname": "father's name",
+    "RegNo": "regno",
+    "Mobile": "***",
+    "Email": "email",
+    "PermanentAddress": "Permanent Address",
+    "FatherMobile": "Fathers Mobile no",
+    "Medium": "English",
+    "CLASS": "3 CS A",
+    "TenthPerc": 90,
+    "twelfPerc": 82.8,
+    "btechAggr": 74.9,
+    "backs": 0
+  },
+{
+    "SerialNo": 1,
+    "Name": "name",
+    "Fname": "father's name",
+    "RegNo": "regno",
+    "Mobile": "***",
+    "Email": "email",
+    "PermanentAddress": "Permanent Address",
+    "FatherMobile": "Fathers Mobile no",
+    "Medium": "English",
+    "CLASS": "3 CS A",
+    "TenthPerc": 90,
+    "twelfPerc": 82.8,
+    "btechAggr": 74.9,
+    "backs": 0
+  }
+];
+
